f0891f9e4fc49145ad906fbf0a1d3a030d4a523b
Krish Manwani <146119587+krishmanwani@users.noreply.github.com>
2025-04-26 10:26:03 +0530

    Delete intracommute-backend directory

--- intracommute-backend/models/Vehicle.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const VehicleSchema = new mongoose.Schema({
-    type: String,
-    isAvailable: Boolean
-});
-
-module.exports = mongoose.model('Vehicle', VehicleSchema);
